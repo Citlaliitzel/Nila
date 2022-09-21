6b9a6a52bb7e001a1e0d24083cb6f2817aa103b8
Citlaliitzel <chica3334@gmail.com>
2022-09-21 10:26:06 -0500

    Objeto response de Ecxpress

--- Parcial 1/ServidorExpress/ServidorExpress.js
+++ Parcial 1/ServidorExpress/ServidorExpress.js
@@ -1,35 +1,33 @@
-var express = require('express')
-//var cors = require('cors')
+const express = require('express')
+const cors = require('cors')
 
-var app = express()
+const app = express()
 
-//app.use(cors({origin: "http://localhost"}))
+app.use(cors({origin: "http://localhost"}))
 //app.use(cors({ origin:"*"}))
 
+//app.get('/', (req, res) => {
+    //res.send('Servidor Express constestando desde Get: PORT8082')
+//})
+
 app.get('/', (req, res) => {
-    res.send('Servidor Express constestando desde Get: PORT8082')
+    res.sendFile('./static/index.html',{root:__dirname},(err)=>{console.log('Archivo enviado')})
 })
 
+//app.post('/', (req, res) => {
+    //res.send('Servidor Express Recibio Post desde el puerto 8082')
+//})
+
 app.post('/', (req, res) => {
-    res.send('Servidor Express Recibio Post desde el puerto 8082')
+    res.send({usuario: 'Nila'})
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile('./static/404.html',{root:__dirname})
 })
 
 app.listen(8082, () => {
     console.log('Servidor Express en Puerto 8082')
+    console.log(__dirname)
+    console.log(__filename)
 })
-
-
-
-
-
-
-
-
-// 1. npm init, para crear los archivos, la carpeta de modulos.
-// 2. npm install express , que este en la carpeta nodule modules, para que nos baje el capeta express
-// 3.- node servidorexpress <- nombre de tu archivo
-
-
-
-// Para instalar el cors: npm install cors
-// C:\xampp\htdocs\hernandezlopezCarlosAndres\Parcial 1\ServidorExpress
