0a1cf6ebf4b32f944d37f8669a37b1fd3c20e098
Citlaliitzel <chica3334@gmail.com>
2022-11-22 08:42:17 -0600

    trabajo clase

---
+++ Parcial 3/jsdoc/src/funciones.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @param {Number} numerobase Numero que se va a elevar
+ * @param {Number} exponente Potencia a la que se va elevar
+ * @returns Number
+ */
+
+export function potencia(numerobase,exponente){
+    return numerobase*exponente;
+}
